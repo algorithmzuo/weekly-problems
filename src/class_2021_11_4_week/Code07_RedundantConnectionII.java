a957a9a5839dc15936eb9e79e35536e3058ba9b6
algorithmzuo <algorithmzuo@163.com>
2021-11-22 14:49:34 +0800

    modify code

--- src/class_2021_11_4_week/Code07_RedundantConnectionII.java
+++ src/class_2021_11_4_week/Code07_RedundantConnectionII.java
@@ -25,21 +25,7 @@ public class Code07_RedundantConnectionII {
 				}
 			}
 		}
-		// circle表示，有没有第一次到达某个to节点，就发现环的情况，有的话记下来，circle = 此时产生环的边
-		// 没有的话，circle维持null的状态
-		// 也就是说，
-		// 客观上如果就没有环，circle == null
-		// 来到某个节点第二次的时候出现环，circle == null(因为只会在第一次来的时候赋值)
-		// 只有第一次来到某个节点的时候发现了环，circle != null(被赋值)
-		// first和second同时设置，要么一起为空，要么一起不为空，表示是否有某个节点入度为2。
-		// first是第一次来到to的边，second是第二次来到to的边
-		// 情况一：没有某个节点入度为2(first == null)。此时要删除的点就是circle。因为只有它多余。
-		// 情况二：有某个节点入度为2(first != null)，且circle != null
-		// 只有来到某个节点第一次的时候，发现了环，circle != null
-		// 那就是first产生的环啊，删掉first
-		// 情况三：有某个节点入度为2(first != null)，且circle == null
-		// 客观上如果就没有环，circle == null，那删除掉second，因为它比first晚出现
-		// 来到某个节点第二次的时候出现环，circle == null，那也删除掉second，因为环就是它产生的
+		// 重点解析！这是啥？？？
 		return first != null ? (circle != null ? first : second) : circle;
 	}
 
