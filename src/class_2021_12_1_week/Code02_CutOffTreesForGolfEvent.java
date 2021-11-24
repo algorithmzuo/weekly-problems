aeb68f66672e5c8c57ce05d8b2d094488d15afbe
algorithmzuo <algorithmzuo@163.com>
2021-11-24 13:27:05 +0800

    modify code

--- src/class_2021_12_1_week/Code02_CutOffTreesForGolfEvent.java
+++ src/class_2021_12_1_week/Code02_CutOffTreesForGolfEvent.java
@@ -1,11 +1,11 @@
-package class_2021_11_4_week;
+package class_2021_12_1_week;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
 
 // 测试链接 : https://leetcode.com/problems/cut-off-trees-for-golf-event/
-public class Code07_CutOffTreesForGolfEvent {
+public class Code02_CutOffTreesForGolfEvent {
 
 	public static int cutOffTree(List<List<Integer>> forest) {
 		int n = forest.size(), m = forest.get(0).size();
