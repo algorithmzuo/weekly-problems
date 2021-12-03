de284815f37448fe0816b0eeb1fd6703c78e1e48
algorithmzuo <algorithmzuo@163.com>
2021-12-03 17:32:57 +0800

    modify code

---
+++ src/class_2021_12_4_week/Code03_DoAllJobs.java
@@ -0,0 +1,113 @@
+package class_2021_12_4_week;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+// 来自hulu
+// 有n个人，m个任务，任务之间有依赖记录在int[][] depends里
+// 比如: depends[i] = [a, b]，表示a任务依赖b任务的完成
+// 其中 0 <= a < m，0 <= b < m
+// 1个人1天可以完成1个任务，每个人都会选当前能做任务里，标号最小的任务
+// 一个任务所依赖的任务都完成了，该任务才能开始做
+// 返回n个人做完m个任务，需要几天
+public class Code03_DoAllJobs {
+
+	public static int days(int n, int m, int[][] depends) {
+		if (n < 1) {
+			return -1;
+		}
+		if (m <= 0) {
+			return 0;
+		}
+		int[][] nexts = nexts(depends, m);
+		int[] indegree = indegree(nexts, m);
+		int[] start = new int[m];
+		PriorityQueue<Integer> workers = new PriorityQueue<>();
+		for (int i = 0; i < n; i++) {
+			workers.add(0);
+		}
+		PriorityQueue<Integer> zeroIn = new PriorityQueue<>();
+		for (int i = 0; i < m; i++) {
+			if (indegree[i] == 0) {
+				zeroIn.add(i);
+			}
+		}
+		int finishAll = 0;
+		int done = 0;
+		while (!zeroIn.isEmpty()) {
+			int job = zeroIn.poll();
+			int wake = workers.poll();
+			int finish = Math.max(start[job], wake) + 1;
+			finishAll = Math.max(finishAll, finish);
+			done++;
+			for (int next : nexts[job]) {
+				start[next] = Math.max(start[next], finish);
+				if (--indegree[next] == 0) {
+					zeroIn.add(next);
+				}
+			}
+			workers.add(finish);
+		}
+		return done == m ? finishAll : -1;
+	}
+
+	public static int[][] nexts(int[][] depends, int m) {
+		Arrays.sort(depends, (a, b) -> a[1] - b[1]);
+		int n = depends.length;
+		int[][] nexts = new int[m][0];
+		if (n == 0) {
+			return nexts;
+		}
+		int size = 1;
+		for (int i = 1; i < n; i++) {
+			if (depends[i - 1][1] != depends[i][1]) {
+				int from = depends[i - 1][1];
+				nexts[from] = new int[size];
+				for (int k = 0, j = i - size; k < size; k++, j++) {
+					nexts[from][k] = depends[j][0];
+				}
+				size = 1;
+			} else {
+				size++;
+			}
+		}
+		int from = depends[n - 1][1];
+		nexts[from] = new int[size];
+		for (int k = 0, j = n - size; k < size; k++, j++) {
+			nexts[from][k] = depends[j][0];
+		}
+		return nexts;
+	}
+
+	public static int[] indegree(int[][] nexts, int m) {
+		int[] indegree = new int[m];
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < nexts[i].length; j++) {
+				indegree[nexts[i][j]]++;
+			}
+		}
+		return indegree;
+	}
+
+	public static void main(String[] args) {
+		// 2 -> 5 -> 7
+		//           |
+		//           v
+		// 1 -> 4 -> 6
+		//      ^
+		//      |
+		// 0 -> 3
+		int[][] d = {
+				{ 3, 0 },
+				{ 4, 1 },
+				{ 5, 2 },
+				{ 4, 3 },
+				{ 7, 5 },
+				{ 6, 4 },
+				{ 6, 7 }
+			};
+		System.out.println(days(3, 8, d));
+		System.out.println(days(2, 8, d));
+	}
+
+}
---
+++ src/class_2021_12_4_week/Code03_WaysToBuildWall.java
@@ -0,0 +1,54 @@
+package class_2021_12_4_week;
+
+// 来自hulu
+// 你只有1*1、1*2、1*3、1*4，四种规格的砖块
+// 你想铺满n行m列的区域，规则如下：
+// 1）不管那种规格的砖，都只能横着摆
+// 比如1*3这种规格的砖，3长度是水平方向，1长度是竖直方向
+// 2）会有很多方法铺满整个区域，整块区域哪怕有一点点不一样，就算不同的方法
+// 3）区域内部(不算区域整体的4条边界)，不能有任何砖块的边界线，是从上一直贯穿到下的直线
+// 返回符合三条规则下，铺满n行m列的区域，有多少种不同的摆放方法
+public class Code03_WaysToBuildWall {
+
+	public static long[] r = { 0, 1, 2, 4, 8 };
+
+	public static long ways(int n, int m) {
+		if (n <= 0 || m <= 1) {
+			return 1;
+		}
+		long[] len = new long[m + 1];
+		for (int i = 1; i <= m; i++) {
+			len[i] = r[i];
+		}
+		for (int i = 5; i <= m; i++) {
+			len[i] = len[i - 1] + len[i - 2] + len[i - 3] + len[i - 4];
+		}
+		long[] any = new long[m + 1];
+		for (int i = 1; i <= m; i++) {
+			any[i] = power(len[i], n);
+		}
+		long[] solid = new long[m + 1];
+		solid[1] = 1;
+		for (int i = 2; i <= m; i++) {
+			long invalid = 0;
+			for (int j = 1; j < i; j++) {
+				invalid += solid[j] * any[i - j];
+			}
+			solid[i] = any[i] - invalid;
+		}
+		return solid[m];
+	}
+
+	public static long power(long base, int power) {
+		long ans = 1;
+		while (power != 0) {
+			if ((power & 1) != 0) {
+				ans *= base;
+			}
+			base *= base;
+			power >>= 1;
+		}
+		return ans;
+	}
+
+}
