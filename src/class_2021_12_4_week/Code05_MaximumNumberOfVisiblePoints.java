15081cee54194eaeb696e3ca985afc3053fa51f4
algorithmzuo <algorithmzuo@163.com>
2021-12-16 23:45:20 +0800

    modify code

---
+++ src/class_2021_12_4_week/Code05_MaximumNumberOfVisiblePoints.java
@@ -0,0 +1,38 @@
+package class_2021_12_4_week;
+
+import java.util.Arrays;
+import java.util.List;
+
+// 测试链接 : https://leetcode.com/problems/maximum-number-of-visible-points/
+public class Code05_MaximumNumberOfVisiblePoints {
+
+	public static int visiblePoints(List<List<Integer>> points, int angle, List<Integer> location) {
+		int n = points.size();
+		int a = location.get(0);
+		int b = location.get(1);
+		int zero = 0;
+		double[] arr = new double[n << 1];
+		int m = 0;
+		for (int i = 0; i < n; i++) {
+			int x = points.get(i).get(0) - a;
+			int y = points.get(i).get(1) - b;
+			if (x == 0 && y == 0) {
+				zero++;
+			} else {
+				arr[m] = Math.toDegrees(Math.atan2(y, x));
+				arr[m + 1] = arr[m] + 360;
+				m += 2;
+			}
+		}
+		Arrays.sort(arr, 0, m);
+		int max = 0;
+		for (int i = 0, j = 0; i < n; i++) {
+			while (j < m && arr[j] - arr[i] <= angle) {
+				j++;
+			}
+			max = Math.max(max, j - i);
+		}
+		return max + zero;
+	}
+
+}
---
+++ src/class_2022_01_4_week/Code02_SplitApples.java
@@ -0,0 +1,53 @@
+package class_2022_01_4_week;
+
+// 测试链接 : https://www.nowcoder.com/practice/bfd8234bb5e84be0b493656e390bdebf
+// 提交以下的code，提交时请把类名改成"Main"
+import java.util.Scanner;
+
+public class Code02_SplitApples {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			int m = sc.nextInt();
+			int n = sc.nextInt();
+			int ways = f(m, n);
+			System.out.println(ways);
+		}
+		sc.close();
+	}
+
+	// 苹果有apples个，盘子有plates个
+	// 返回有几种摆法
+	// 如果苹果数为0，有1种摆法：什么也不摆
+	// 如果苹果数不为0，但是盘子数为0，有0种摆法（做不到）
+	// 如果苹果数不为0，盘子数也不为0，进行如下的情况讨论：
+	// 假设苹果数为apples，盘子数为plates
+	// 可能性 1) apples < plates
+	// 这种情况下，一定有多余的盘子，这些盘子完全没用，所以砍掉
+	// 后续是f(apples, apples)
+	// 可能性 2) apples >= plates
+	// 在可能性2)下，讨论摆法，有如下两种选择
+	// 选择a) 会有空盘
+	// 选择b) 不再有空盘
+	// 对于选择a)，既然有空盘，那么后续就是f(apples, plates - 1)
+	// 意思是：不是说有空盘嘛？那盘子减少一个，然后继续讨论吧！
+	// 对于选择b)，既然不再有空盘，那么先把所有盘子都摆上1个苹果。
+	// 剩余苹果数 = apples - plates
+	// 然后继续讨论，剩下的这些苹果，怎么摆进plates个盘子里，
+	// 所以后续是f(apples - plates, plates)
+	public static int f(int apples, int plates) {
+		if (apples == 0) {
+			return 1;
+		}
+		if (plates == 0) {
+			return 0;
+		}
+		if (plates > apples) {
+			return f(apples, apples);
+		} else { // apples >= plates;
+			return f(apples, plates - 1) + f(apples - plates, plates);
+		}
+	}
+
+}
