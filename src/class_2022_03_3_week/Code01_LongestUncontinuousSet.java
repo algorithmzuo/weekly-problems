9956a09a2eb1fab000a10c5d8b6b1bb554ecdc31
algorithmzuo <algorithmzuo@163.com>
2022-03-16 19:43:13 +0800

    modify code

--- src/class_2022_03_3_week/Code01_LongestUncontinuousSet.java
+++ src/class_2022_03_3_week/Code01_LongestUncontinuousSet.java
@@ -8,7 +8,34 @@ import java.util.Arrays;
 // 返回你最多能挑选几个数
 public class Code01_LongestUncontinuousSet {
 
-	public static int longestUncontinuous(int[] arr) {
+	// 暴力方法
+	// 为了验证
+	public static int longestUncontinuous1(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			return 0;
+		}
+		Arrays.sort(arr);
+		return process1(arr, 0, new int[arr.length], 0);
+	}
+
+	public static int process1(int[] arr, int i, int[] path, int j) {
+		if (i == arr.length) {
+			for (int k = 1; k < j; k++) {
+				if (path[k - 1] + 1 >= path[k]) {
+					return 0;
+				}
+			}
+			return j;
+		} else {
+			int p1 = process1(arr, i + 1, path, j);
+			path[j] = arr[i];
+			int p2 = process1(arr, i + 1, path, j + 1);
+			return Math.max(p1, p2);
+		}
+	}
+
+	// 最优解
+	public static int longestUncontinuous2(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return 0;
 		}
@@ -34,6 +61,40 @@ public class Code01_LongestUncontinuousSet {
 			ans = Math.max(ans, dp[i]);
 		}
 		return ans;
+	}
+
+	// 为了测试
+	public static int[] randomArray(int n, int v) {
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = (int) (Math.random() * v) - (int) (Math.random() * v);
+		}
+		return arr;
+	}
+
+	// 为了测试
+	public static void main(String[] args) {
+		int len = 10;
+		int value = 20;
+		int testTime = 2000;
+		System.out.println("测试开始");
+		for (int i = 0; i < testTime; i++) {
+			int n = (int) (Math.random() * len) + 1;
+			int[] arr = randomArray(n, value);
+			int ans1 = longestUncontinuous1(arr);
+			int ans2 = longestUncontinuous2(arr);
+			if (ans1 != ans2) {
+				System.out.println("出错了!");
+				for (int num : arr) {
+					System.out.print(num + " ");
+				}
+				System.out.println();
+				System.out.println(ans1);
+				System.out.println(ans2);
+				break;
+			}
+		}
+		System.out.println("测试结束");
 	}
 
 }
