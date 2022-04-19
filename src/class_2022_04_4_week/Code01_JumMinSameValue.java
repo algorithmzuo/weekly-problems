0e6090efd2863d02859bae5ce45b45bf80476dc9
algorithmzuo <algorithmzuo@163.com>
2022-04-19 13:43:57 +0800

    modify code

--- src/class_2022_04_4_week/Code01_JumMinSameValue.java
+++ src/class_2022_04_4_week/Code01_JumMinSameValue.java
@@ -3,6 +3,7 @@ package class_2022_04_4_week;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+// 来自蔚来汽车
 // 给你一个整数数组 arr ，你一开始在数组的第一个元素处（下标为 0）。
 // 每一步，你可以从下标 i 跳到下标 i + 1 、i - 1 或者 j ：
 // i + 1 需满足：i + 1 < arr.length
@@ -10,7 +11,7 @@ import java.util.HashMap;
 // j 需满足：arr[i] == arr[j] 且 i != j
 // 请你返回到达数组最后一个元素的下标处所需的 最少操作次数 。
 // 注意：任何时候你都不能跳到数组外面。
-// 本题测试链接 : https://leetcode-cn.com/problems/jump-game-iv/
+// leetcode测试链接 : https://leetcode-cn.com/problems/jump-game-iv/
 public class Code01_JumMinSameValue {
 
 	public static int minJumps(int[] arr) {
