b941bb7df7c6b5b8455a68880b3f87386d611753
algorithmzuo <algorithmzuo@163.com>
2022-06-08 14:43:59 +0800

    modify code

--- src/class_2022_06_1_week/Code04_SubstringWithLargestVariance.java
+++ src/class_2022_06_1_week/Code04_SubstringWithLargestVariance.java
@@ -3,7 +3,45 @@ package class_2022_06_1_week;
 // 测试链接 : https://leetcode.cn/problems/substring-with-largest-variance/
 public class Code04_SubstringWithLargestVariance {
 
-	public static int largestVariance(String s) {
+	public static int largestVariance1(String s) {
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+		int n = s.length();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = s.charAt(i) - 'a';
+		}
+		int ans = 0;
+		for (int a = 0; a < 26; a++) {
+			for (int b = 0; b < 26; b++) {
+				if (a != b) {
+					int continuousA = 0;
+					boolean appearB = false;
+					int max = 0;
+					for (int i = 0; i < n; i++) {
+						if (arr[i] != a && arr[i] != b) {
+							continue;
+						}
+						if (arr[i] == a) {
+							continuousA++;
+							if (appearB) {
+								max++;
+							}
+						} else {
+							max = Math.max(max, continuousA) - 1;
+							continuousA = 0;
+							appearB = true;
+						}
+						ans = Math.max(ans, max);
+					}
+				}
+			}
+		}
+		return ans;
+	}
+
+	public static int largestVariance2(String s) {
 		if (s == null || s.length() == 0) {
 			return 0;
 		}
