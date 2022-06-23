09663bf70aa7b54d788c619aba3e8b4c613733ef
algorithmzuo <algorithmzuo@163.com>
2022-06-23 14:14:49 +0800

    modify code

--- src/class_2022_06_3_week/Code03_RangeModule.java
+++ src/class_2022_06_3_week/Code03_RangeModule.java
@@ -5,6 +5,20 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
+// Range模块是跟踪数字范围的模块。
+// 设计一个数据结构来跟踪表示为 半开区间 的范围并查询它们。
+// 半开区间 [left, right) 表示所有 left <= x < right 的实数 x 。
+// 实现 RangeModule 类:
+// RangeModule() 初始化数据结构的对象
+// void addRange(int left, int right) : 
+// 添加 半开区间 [left, right)，跟踪该区间中的每个实数。
+// 添加与当前跟踪的数字部分重叠的区间时，
+// 应当添加在区间 [left, right) 中尚未跟踪的任何数字到该区间中。
+// boolean queryRange(int left, int right) : 
+// 只有在当前正在跟踪区间 [left, right) 中的每一个实数时，才返回 true
+// 否则返回 false 。
+// void removeRange(int left, int right) : 
+// 停止跟踪 半开区间 [left, right) 中当前正在跟踪的每个实数。
 // 测试连接 : https://leetcode.com/problems/range-module/
 public class Code03_RangeModule {
 
@@ -170,16 +184,13 @@ public class Code03_RangeModule {
 		map.put(4, "我是9");
 		// 3 4 5 6 9
 		// [4~6) -> 4, 5,6
-		
-		
-		
+
 		Map<Integer, String> subMap = map.subMap(4, true, 6, false);
-		
-		for(int key : subMap.keySet()) {
+
+		for (int key : subMap.keySet()) {
 			System.out.println(key);
 		}
-		
-		
+
 	}
 
 }
