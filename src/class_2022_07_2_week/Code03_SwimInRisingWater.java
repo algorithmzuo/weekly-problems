046d42217a73ae26193e4c3bf8703bb4e7629e44
algorithmzuo <algorithmzuo@163.com>
2022-07-20 11:35:32 +0800

    modify code

--- src/class_2022_07_2_week/Code03_SwimInRisingWater.java
+++ src/class_2022_07_2_week/Code03_SwimInRisingWater.java
@@ -3,7 +3,15 @@ package class_2022_07_2_week;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
-// Leetcode测试链接 : https://leetcode.cn/problems/swim-in-rising-water/
+// 在一个 n x n 的整数矩阵 grid 中，
+// 每一个方格的值 grid[i][j] 表示位置 (i, j) 的平台高度。
+// 当开始下雨时，在时间为 t 时，水池中的水位为 t 。
+// 你可以从一个平台游向四周相邻的任意一个平台，但是前提是此时水位必须同时淹没这两个平台。
+// 假定你可以瞬间移动无限距离，也就是默认在方格内部游动是不耗时的。
+// 当然，在你游泳的时候你必须待在坐标方格里面。
+// 你从坐标方格的左上平台 (0，0) 出发。
+// 返回 你到达坐标方格的右下平台 (n-1, n-1) 所需的最少时间 。
+// 测试链接 ：https://leetcode.cn/problems/swim-in-rising-water
 public class Code03_SwimInRisingWater {
 
 	// 并查集的解法
