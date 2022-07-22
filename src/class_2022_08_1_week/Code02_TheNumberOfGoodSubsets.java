bd1ba4c1c562616e1f14a0ab52808c034574886d
algorithmzuo <algorithmzuo@163.com>
2022-07-23 01:53:01 +0800

    modify code

--- src/class_2022_08_1_week/Code02_TheNumberOfGoodSubsets.java
+++ src/class_2022_08_1_week/Code02_TheNumberOfGoodSubsets.java
@@ -5,40 +5,41 @@ import java.util.Arrays;
 // 测试链接 : https://leetcode.cn/problems/the-number-of-good-subsets/
 public class Code02_TheNumberOfGoodSubsets {
 
-	// 2,3,5,6,7,10,11,13,14,15,17,19,21,22,23,26,29,30
-	public static int[] numberPrimes = { 0, 0, 1, 2, 0, 4, 3, 8, 0, 0, 5, 16, 0, 32, 9, 6, 0, 64, 0, 128, 0, 10, 17,
-			256, 0, 0, 33, 0, 0, 512, 7 };
-	// v = 0110 0100 1110 1010 1110 1100 1110 1100;
-	public static int okNumbers = 0x64eaecec;
-	public static int valueMax = 30;
-	public static int statusMax = 1 << 10;
-	public static int[] cnt = new int[valueMax + 1];
-	public static int[] status = new int[statusMax];
+	// 2, 3, 5, 6, 7, 10, 11, 13, 14,
+	// 15, 17, 19, 21, 22, 23, 26, 29, 30
+	public static int[] primes = { 
+			0, 0, 1, 2, 0, 4, 3, 8, 0, 0,
+			5, 16, 0, 32, 9, 6, 0, 64, 0, 128,
+			0, 10, 17, 256, 0, 0, 33, 0, 0, 512, 7 };
+
+	public static int[] counts = new int[31];
+
+	public static int[] status = new int[1 << 10];
+
 	public static int mod = 1000000007;
 
 	public static int numberOfGoodSubsets(int[] nums) {
-		Arrays.fill(cnt, 0);
+		Arrays.fill(counts, 0);
 		Arrays.fill(status, 0);
 		for (int num : nums) {
-			cnt[num]++;
+			counts[num]++;
 		}
 		status[0] = 1;
-		for (int i = 0; i < cnt[1]; i++) {
+		for (int i = 0; i < counts[1]; i++) {
 			status[0] = (status[0] << 1) % mod;
 		}
-		for (int i = 2; i <= valueMax; i++) {
-			if ((okNumbers & (1 << i)) == 0 || cnt[i] == 0) {
-				continue;
-			}
-			int cur = numberPrimes[i];
-			for (int mask = 0; mask < statusMax; mask++) {
-				if ((mask & cur) == 0) {
-					status[mask | cur] = (int) ((status[mask | cur] + ((long) status[mask] * cnt[i])) % mod);
+		for (int i = 2; i <= 30; i++) {
+			int cur = primes[i];
+			if (cur != 0 && counts[i] != 0) {
+				for (int mask = 0; mask < (1 << 10); mask++) {
+					if ((mask & cur) == 0) {
+						status[mask | cur] =  (int) (((long) status[mask | cur]  + ((long) status[mask] * counts[i])) % mod);
+					}
 				}
 			}
 		}
 		int ans = 0;
-		for (int mask = 1; mask < statusMax; mask++) {
+		for (int mask = 1; mask < (1 << 10); mask++) {
 			ans = (ans + status[mask]) % mod;
 		}
 		return ans;
