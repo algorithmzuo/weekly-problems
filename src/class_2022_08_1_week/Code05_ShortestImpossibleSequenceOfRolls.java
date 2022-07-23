c797b30a977b7e462919f6ea277ca9e70edaccc8
algorithmzuo <algorithmzuo@163.com>
2022-07-24 00:58:37 +0800

    modify code

---
+++ src/class_2022_08_1_week/Code05_ShortestImpossibleSequenceOfRolls.java
@@ -0,0 +1,26 @@
+package class_2022_08_1_week;
+
+import java.util.Arrays;
+
+// 测试链接 : https://leetcode.cn/problems/shortest-impossible-sequence-of-rolls/
+public class Code05_ShortestImpossibleSequenceOfRolls {
+
+	public static int shortestSequence(int[] rolls, int k) {
+		boolean[] set = new boolean[k + 1];
+		int size = 0;
+		int ans = 0;
+		for (int num : rolls) {
+			if (!set[num]) {
+				set[num] = true;
+				size++;
+			}
+			if (size == k) {
+				ans++;
+				Arrays.fill(set, false);
+                size = 0;
+			}
+		}
+		return ans + 1;
+	}
+
+}
