bc1a7f9cb86b88be5bca890426f8215e7ed4c03a
algorithmzuo <algorithmzuo@163.com>
2022-08-03 19:46:23 +0800

    modify code

--- src/class_2022_08_1_week/Code03_OnlineMajorityElementInSubarray.java
+++ src/class_2022_08_1_week/Code03_OnlineMajorityElementInSubarray.java
@@ -2,6 +2,14 @@ package class_2022_08_1_week;
 
 import java.util.ArrayList;
 
+// 设计一个数据结构，有效地找到给定子数组的 多数元素 。
+// 子数组的 多数元素 是在子数组中出现 threshold 次数或次数以上的元素。
+// 实现 MajorityChecker 类:
+// MajorityChecker(int[] arr) 
+// 会用给定的数组 arr 对 MajorityChecker 初始化。
+// int query(int left, int right, int threshold) 
+// 返回子数组中的元素  arr[left...right] 至少出现 threshold 次数，
+// 如果不存在这样的元素则返回 -1。
 // 为了让同学们听懂这个题
 // 才安排的水王问题重讲
 // 测试链接 : https://leetcode.cn/problems/online-majority-element-in-subarray/
--- src/class_2022_08_1_week/Code04_ShortestImpossibleSequenceOfRolls.java
+++ src/class_2022_08_1_week/Code04_ShortestImpossibleSequenceOfRolls.java
@@ -2,6 +2,12 @@ package class_2022_08_1_week;
 
 import java.util.Arrays;
 
+// 给你一个长度为 n 的整数数组 rolls 和一个整数 k 。
+// 你扔一个 k 面的骰子 n 次，骰子的每个面分别是 1 到 k ，
+// 其中第 i 次扔得到的数字是 rolls[i] 。
+// 请你返回 无法 从 rolls 中得到的 最短 骰子子序列的长度。
+// 扔一个 k 面的骰子 len 次得到的是一个长度为 len 的 骰子子序列 。
+// 注意 ，子序列只需要保持在原数组中的顺序，不需要连续。
 // 测试链接 : https://leetcode.cn/problems/shortest-impossible-sequence-of-rolls/
 public class Code04_ShortestImpossibleSequenceOfRolls {
 
