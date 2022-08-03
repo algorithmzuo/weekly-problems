3a3f6500b4a1fba553002b513b70d9419f3d83af
algorithmzuo <algorithmzuo@163.com>
2022-08-03 19:39:03 +0800

    modify code

--- src/class_2022_08_1_week/Code05_LongestOneLetterManyNumberString.java
+++ src/class_2022_08_1_week/Code05_LongestOneLetterManyNumberString.java
@@ -1,9 +1,9 @@
-package class_2022_08_4_week;
+package class_2022_08_1_week;
 
 // 给定一个只由小写字母和数字字符组成的字符串str
 // 要求子串必须只含有一个小写字母，数字字符数量随意
 // 求这样的子串最大长度是多少
-public class Code01_LongestOneLetterManyNumberString {
+public class Code05_LongestOneLetterManyNumberString {
 
 	// 一个绝对正确的暴力方法
 	public static int right(String s) {
