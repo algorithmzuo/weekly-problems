43c40ffdd7cf9dcb82a4b3971df8b11d9c8f5a4b
algorithmzuo <algorithmzuo@163.com>
2022-08-08 23:40:14 +0800

    modify code

--- src/class_2022_08_4_week/Code03_CountDifferentPalindromicSubsequences.java
+++ src/class_2022_08_4_week/Code03_CountDifferentPalindromicSubsequences.java
@@ -1,35 +1,28 @@
 package class_2022_08_4_week;
 
 import java.util.HashMap;
-import java.util.TreeSet;
 
 // 测试链接 : https://leetcode.cn/problems/count-different-palindromic-subsequences/
 public class Code03_CountDifferentPalindromicSubsequences {
 
-	public static int mod = 1000000007;
-
-	// 需要限定字符集
+	// 限定字符集(a、b、c、d)
 	// 时间复杂度O(N^2)
 	public static int countPalindromicSubsequences1(String str) {
+		int mod = 1000000007;
 		char[] s = str.toCharArray();
 		int n = s.length;
-		int[][] right = new int[4][n];
-		int[][] left = new int[4][n];
+		int[] right = new int[n];
+		int[] left = new int[n];
 		int[] last = new int[4];
 		last[0] = last[1] = last[2] = last[3] = -1;
 		for (int i = 0; i < n; i++) {
+			left[i] = last[s[i] - 'a'];
 			last[s[i] - 'a'] = i;
-			for (int c = 0; c < 4; c++) {
-				left[c][i] = last[c];
-			}
-
 		}
 		last[0] = last[1] = last[2] = last[3] = n;
 		for (int i = n - 1; i >= 0; i--) {
+			right[i] = last[s[i] - 'a'];
 			last[s[i] - 'a'] = i;
-			for (int c = 0; c < 4; c++) {
-				right[c][i] = last[c];
-			}
 		}
 		long[][] dp = new long[n][n];
 		for (int i = 0; i < n; i++) {
@@ -38,14 +31,8 @@ public class Code03_CountDifferentPalindromicSubsequences {
 		for (int d = 1; d < n; d++) {
 			for (int i = 0; i < n - d; i++) {
 				if (s[i] == s[i + d]) {
-					int l = i + d;
-					if (right[s[i] - 'a'][i + 1] != n) {
-						l = right[s[i] - 'a'][i + 1];
-					}
-					int r = i;
-					if (left[s[i] - 'a'][i + d - 1] != -1) {
-						r = left[s[i] - 'a'][i + d - 1];
-					}
+					int l = Math.min(i + d, right[i]);
+					int r = Math.max(i, left[i + d]);
 					if (l > r) {
 						dp[i][i + d] = dp[i][i + d] = dp[i + 1][i + d - 1] * 2 + 2;
 					} else if (l == r) {
@@ -63,16 +50,22 @@ public class Code03_CountDifferentPalindromicSubsequences {
 	}
 
 	// 不需要限定字符集
-	// 时间复杂度O(N^2 * logN)
+	// 时间复杂度O(N^2)
 	public static int countPalindromicSubsequences2(String str) {
+		int mod = 1000000007;
 		char[] s = str.toCharArray();
 		int n = s.length;
-		HashMap<Character, TreeSet<Integer>> map = new HashMap<>();
+		HashMap<Integer, Integer> right = new HashMap<>();
+		HashMap<Integer, Integer> left = new HashMap<>();
+		HashMap<Character, Integer> last = new HashMap<>();
 		for (int i = 0; i < n; i++) {
-			if (!map.containsKey(s[i])) {
-				map.put(s[i], new TreeSet<>());
-			}
-			map.get(s[i]).add(i);
+			left.put(i, last.getOrDefault(s[i], -1));
+			last.put(s[i], i);
+		}
+		last.clear();
+		for (int i = n - 1; i >= 0; i--) {
+			right.put(i, last.getOrDefault(s[i], n));
+			last.put(s[i], i);
 		}
 		long[][] dp = new long[n][n];
 		for (int i = 0; i < n; i++) {
@@ -81,16 +74,8 @@ public class Code03_CountDifferentPalindromicSubsequences {
 		for (int d = 1; d < n; d++) {
 			for (int i = 0; i < n - d; i++) {
 				if (s[i] == s[i + d]) {
-					int l = i + d;
-					Integer ceiling = map.get(s[i]).ceiling(i + 1);
-					if (ceiling != null) {
-						l = ceiling;
-					}
-					int r = i;
-					Integer floor = map.get(s[i]).floor(i + d - 1);
-					if (floor != null) {
-						r = floor;
-					}
+					int l = Math.min(i + d, right.get(i));
+					int r = Math.max(i, left.get(i + d));
 					if (l > r) {
 						dp[i][i + d] = dp[i][i + d] = dp[i + 1][i + d - 1] * 2 + 2;
 					} else if (l == r) {
