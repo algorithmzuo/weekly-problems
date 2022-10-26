73f17c277a617d161d6d0a92ce49533fb4a7b75f
algorithmzuo <algorithmzuo@163.com>
2022-10-26 19:58:13 +0800

    modify code

--- src/class_2022_11_1_week/Code02_AbsToArrayFinalLength.java
+++ src/class_2022_11_1_week/Code02_AbsToArrayFinalLength.java
@@ -1,15 +1,17 @@
 package class_2022_11_1_week;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 // 来自国外题目论坛
-// 给定一个去重数组arr，任何两个数差值的绝对值都要加入到arr里
-// 然后新的arr，继续任何两个数差值的绝对值都要加入到arr里
+// 给定一个非负数组arr
+// 继续任何两个数差值的绝对值，如果arr中没有，都要加入到arr里
+// 然后新的arr，继续任何两个数差值的绝对值，如果arr中没有，都要加入到arr里
 // 一直到arr大小固定
 // 请问最终arr长度是多少
 // 1 <= arr的长度 <= 10^5
-// 1 <= arr的数值 <= 10^5
+// 0 <= arr的数值 <= 10^5
 public class Code02_AbsToArrayFinalLength {
 
 	// 暴力方法
@@ -44,14 +46,36 @@ public class Code02_AbsToArrayFinalLength {
 	// 时间复杂O(N)
 	public static int finalLen2(int[] arr) {
 		int max = 0;
+		int gcd = 0;
 		for (int num : arr) {
 			max = Math.max(max, num);
+			if (num != 0) {
+				gcd = num;
+			}
 		}
-		int gcd = arr[0];
+		if (gcd == 0) {
+			return arr.length;
+		}
+		HashMap<Integer, Integer> counts = new HashMap<>();
 		for (int num : arr) {
-			gcd = gcd(gcd, num);
+			if (num != 0) {
+				gcd = gcd(gcd, num);
+			}
+			counts.put(num, counts.getOrDefault(num, 0) + 1);
 		}
-		return max / gcd;
+		int ans = max / gcd;
+		ans += counts.getOrDefault(0, 0);
+		boolean add = false;
+		for (int key : counts.keySet()) {
+			if (key != 0) {
+				ans += counts.get(key) - 1;
+			}
+			if (counts.get(key) > 1 && !counts.containsKey(0) && !add) {
+				ans++;
+				add = true;
+			}
+		}
+		return ans;
 	}
 
 	public static int gcd(int m, int n) {
@@ -61,12 +85,8 @@ public class Code02_AbsToArrayFinalLength {
 	// 为了测试
 	public static int[] randomArray(int n, int v) {
 		int[] ans = new int[n];
-		HashSet<Integer> set = new HashSet<>();
 		for (int i = 0; i < n; i++) {
-			do {
-				ans[i] = (int) (Math.random() * v) + 1;
-			} while (set.contains(ans[i]));
-			set.add(ans[i]);
+			ans[i] = (int) (Math.random() * v);
 		}
 		return ans;
 	}
@@ -74,7 +94,7 @@ public class Code02_AbsToArrayFinalLength {
 	// 为了测试
 	public static void main(String[] args) {
 		int N = 10;
-		int V = 20;
+		int V = 10;
 		int testTime = 5000;
 		System.out.println("功能测试开始");
 		for (int i = 0; i < testTime; i++) {
@@ -83,6 +103,9 @@ public class Code02_AbsToArrayFinalLength {
 			int ans1 = finalLen1(arr);
 			int ans2 = finalLen2(arr);
 			if (ans1 != ans2) {
+				for (int num : arr) {
+					System.out.print(num + " ");
+				}
 				System.out.println("出错了!");
 			}
 		}
