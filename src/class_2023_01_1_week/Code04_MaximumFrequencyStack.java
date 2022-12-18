2b827e427e9c6c51c90b739045ade7b652422d71
algorithmzuo <algorithmzuo@163.com>
2022-12-18 17:30:54 +0800

    modify code

--- src/class_2023_01_1_week/Code04_MaximumFrequencyStack.java
+++ src/class_2023_01_1_week/Code04_MaximumFrequencyStack.java
@@ -8,125 +8,38 @@ public class Code04_MaximumFrequencyStack {
 
 	class FreqStack {
 
-		public static class Node {
-			public int val;
-			public ArrayList<Integer> whenList;
-			public int size;
+		private int topTimes;
 
-			public Node(int v, int w) {
-				val = v;
-				whenList = new ArrayList<>();
-				whenList.add(w);
-				size = 1;
-			}
+		private HashMap<Integer, ArrayList<Integer>> cntValues = new HashMap<>();
 
-			public void addOnce(int w) {
-				if (whenList.size() == size) {
-					whenList.add(w);
-				} else {
-					whenList.set(size, w);
-				}
-				size++;
-			}
-
-			public void minusOnce() {
-				size--;
-			}
-
-			public int times() {
-				return size;
-			}
-
-			public int last() {
-				return whenList.get(size - 1);
-			}
-
-		}
-
-		public int timestamp;
-
-		public ArrayList<Node> heap;
-
-		public HashMap<Integer, Integer> indies;
-
-		public int heapSize;
-
-		public FreqStack() {
-			heap = new ArrayList<>();
-			indies = new HashMap<>();
-			timestamp = 0;
-			heapSize = 0;
-		}
+		private HashMap<Integer, Integer> valueTopTime = new HashMap<>();
 
 		public void push(int val) {
-			if (!indies.containsKey(val)) {
-				Node newData = new Node(val, timestamp++);
-				if (heap.size() == heapSize) {
-					heap.add(newData);
-				} else {
-					heap.set(heapSize, newData);
-				}
-				indies.put(val, heapSize);
-				heapInsert(heapSize++);
-			} else {
-				int index = indies.get(val);
-				Node data = heap.get(index);
-				data.addOnce(timestamp++);
-				resign(index);
+			valueTopTime.put(val, valueTopTime.getOrDefault(val, 0) + 1);
+			int curTopTimes = valueTopTime.get(val);
+			if (!cntValues.containsKey(curTopTimes)) {
+				cntValues.put(curTopTimes, new ArrayList<>());
 			}
+			ArrayList<Integer> curTimeValues = cntValues.get(curTopTimes);
+			curTimeValues.add(val);
+			topTimes = Math.max(topTimes, curTopTimes);
 		}
 
 		public int pop() {
-			Node head = heap.get(0);
-			if (head.times() == 1) {
-				swap(0, --heapSize);
-				indies.remove(head.val);
+			ArrayList<Integer> topTimeValues = cntValues.get(topTimes);
+			int last = topTimeValues.size() - 1;
+			int ans = topTimeValues.remove(last);
+			if (topTimeValues.size() == 0) {
+				cntValues.remove(topTimes--);
+			}
+			int times = valueTopTime.get(ans);
+			if (times == 1) {
+				valueTopTime.remove(ans);
 			} else {
-				head.minusOnce();
+				valueTopTime.put(ans, times - 1);
 			}
-			heapify(0);
-			return head.val;
+			return ans;
 		}
-
-		private void resign(int i) {
-			heapInsert(i);
-			heapify(i);
-		}
-
-		private void heapInsert(int i) {
-			while (compare(heap.get(i), heap.get((i - 1) / 2)) < 0) {
-				swap(i, (i - 1) / 2);
-				i = (i - 1) / 2;
-			}
-		}
-
-		private void heapify(int i) {
-			int l = i * 2 + 1;
-			while (l < heapSize) {
-				int best = l + 1 < heapSize && compare(heap.get(l + 1), heap.get(l)) < 0 ? (l + 1) : l;
-				best = compare(heap.get(best), heap.get(i)) < 0 ? best : i;
-				if (best == i) {
-					break;
-				}
-				swap(best, i);
-				i = best;
-				l = i * 2 + 1;
-			}
-		}
-
-		private int compare(Node a, Node b) {
-			return a.times() != b.times() ? (b.times() - a.times()) : (b.last() - a.last());
-		}
-
-		private void swap(int i, int j) {
-			Node o1 = heap.get(i);
-			Node o2 = heap.get(j);
-			heap.set(i, o2);
-			heap.set(j, o1);
-			indies.put(o2.val, i);
-			indies.put(o1.val, j);
-		}
-
 	}
 
 }
