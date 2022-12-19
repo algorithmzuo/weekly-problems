690ac0178f14f3ee415e6b79446a768696556a9d
algorithmzuo <algorithmzuo@163.com>
2022-12-19 21:32:59 +0800

    modify code

---
+++ src/class_2023_01_3_week/Code01_SuperPalindromes.java
@@ -0,0 +1,65 @@
+package class_2023_01_3_week;
+
+// 测试链接 : https://leetcode.cn/problems/super-palindromes/
+public class Code01_SuperPalindromes {
+
+	public static int superpalindromesInRange(String left, String right) {
+		long l = Long.valueOf(left);
+		long r = Long.valueOf(right);
+		long sqrtR = (long) Math.sqrt((double) r);
+		int cnt = 0;
+		long seed = 1;
+		long enlarge = 0;
+		do {
+			enlarge = enlarge2(seed);
+			if (isValid(enlarge * enlarge, l, r)) {
+				cnt++;
+			}
+			enlarge = enlarge1(seed);
+			if (isValid(enlarge * enlarge, l, r)) {
+				cnt++;
+			}
+			seed++;
+		} while (enlarge < sqrtR);
+		return cnt;
+	}
+
+	public static long enlarge1(long seed) {
+		long ans = seed;
+		seed /= 10;
+		while (seed != 0) {
+			ans = ans * 10 + seed % 10;
+			seed /= 10;
+		}
+		return ans;
+	}
+
+	public static long enlarge2(long seed) {
+		long ans = seed;
+		while (seed != 0) {
+			ans = ans * 10 + seed % 10;
+			seed /= 10;
+		}
+		return ans;
+	}
+
+	public static boolean isValid(long ans, long l, long r) {
+		return isPalindrome(ans) && ans >= l && ans <= r;
+	}
+
+	public static boolean isPalindrome(long n) {
+		long help = 1;
+		while (n / help >= 10) {
+			help *= 10;
+		}
+		while (n != 0) {
+			if (n / help != n % 10) {
+				return false;
+			}
+			n = (n % help) / 10;
+			help /= 100;
+		}
+		return true;
+	}
+
+}
