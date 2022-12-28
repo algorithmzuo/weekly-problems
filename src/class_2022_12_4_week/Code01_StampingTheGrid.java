01beda42c0f86b30f94c93dc734da7c0b2953a83
algorithmzuo <algorithmzuo@163.com>
2022-12-28 19:47:59 +0800

    modify code

--- src/class_2022_12_4_week/Code01_StampingTheGrid.java
+++ src/class_2022_12_4_week/Code01_StampingTheGrid.java
@@ -1,5 +1,13 @@
 package class_2022_12_4_week;
 
+// 给你一个 m x n 的二进制矩阵 grid
+// 每个格子要么为 0 （空）要么为 1 （被占据）
+// 给你邮票的尺寸为 stampHeight x stampWidth
+// 我们想将邮票贴进二进制矩阵中，且满足以下 限制 和 要求 ：
+// 覆盖所有空格子，不覆盖任何被占据的格子
+// 可以放入任意数目的邮票，邮票可以相互有重叠部分
+// 邮票不允许旋转，邮票必须完全在矩阵内
+// 如果在满足上述要求的前提下，可以放入邮票，请返回 true ，否则返回 false
 // 测试链接 : https://leetcode.cn/problems/stamping-the-grid/
 public class Code01_StampingTheGrid {
 
