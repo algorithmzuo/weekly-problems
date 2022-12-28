64d0a6ad3c39447222908b9d44419cd5ca13d1a8
algorithmzuo <algorithmzuo@163.com>
2022-12-28 13:39:28 +0800

    modify code

--- src/class_2022_12_4_week/Code02_ShortestPathVisitingAllNodes.java
+++ src/class_2022_12_4_week/Code02_ShortestPathVisitingAllNodes.java
@@ -1,6 +1,11 @@
 package class_2022_12_4_week;
 
 // Floyd算法应用
+// 存在一个由 n 个节点组成的无向连通图，图中的节点按从 0 到 n - 1 编号
+// 给你一个数组 graph 表示这个图
+// 其中，graph[i] 是一个列表，由所有与节点 i 直接相连的节点组成
+// 返回能够访问所有节点的最短路径的长度
+// 你可以在任一节点开始和停止，也可以多次重访节点，并且可以重用边
 // 测试链接 : https://leetcode.cn/problems/shortest-path-visiting-all-nodes/
 public class Code02_ShortestPathVisitingAllNodes {
 
--- src/class_2022_12_4_week/Code05_KSimilarStrings.java
+++ src/class_2022_12_4_week/Code05_KSimilarStrings.java
@@ -3,6 +3,9 @@ package class_2022_12_4_week;
 import java.util.HashSet;
 import java.util.PriorityQueue;
 
+// 对于某些非负整数 k ，如果交换 s1 中两个字母的位置恰好 k 次
+// 能够使结果字符串等于 s2 ，则认为字符串 s1 和 s2 的 相似度为 k
+// 给你两个字母异位词 s1 和 s2 ，返回 s1 和 s2 的相似度 k 的最小值
 // 测试链接 : https://leetcode.cn/problems/k-similar-strings/
 public class Code05_KSimilarStrings {
 
