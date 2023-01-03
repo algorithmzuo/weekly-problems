71166e7c7fa566165867b902e65f52287e38a7ee
algorithmzuo <algorithmzuo@163.com>
2023-01-03 16:35:38 +0800

    modify code

---
+++ src/class_2023_02_1_week/Code01_LeastOperatorsToExpressNumber.java
@@ -0,0 +1,37 @@
+package class_2023_02_1_week;
+
+import java.util.HashMap;
+
+// 测试链接 : https://leetcode.cn/problems/least-operators-to-express-number/
+public class Code01_LeastOperatorsToExpressNumber {
+
+	public static int leastOpsExpressTarget(int x, int target) {
+		return dp(0, target, x, new HashMap<>()) - 1;
+	}
+
+	public static int dp(int i, int target, int x, HashMap<Integer, HashMap<Integer, Integer>> dp) {
+		if (dp.containsKey(i) && dp.get(i).containsKey(target)) {
+			return dp.get(i).get(target);
+		}
+		int ans = 0;
+		if (target > 0 && i < 39) {
+			if (target == 1) {
+				ans = cost(i);
+			} else {
+				int t = target / x;
+				int r = target % x;
+				ans = Math.min(r * cost(i) + dp(i + 1, t, x, dp), (x - r) * cost(i) + dp(i + 1, t + 1, x, dp));
+			}
+		}
+		if (!dp.containsKey(i)) {
+			dp.put(i, new HashMap<>());
+		}
+		dp.get(i).put(target, ans);
+		return ans;
+	}
+
+	public static int cost(int x) {
+		return x > 0 ? x : 2;
+	}
+
+}
