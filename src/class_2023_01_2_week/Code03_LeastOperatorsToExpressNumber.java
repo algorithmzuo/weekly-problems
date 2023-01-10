cad243bf7606ec7f5bfa239c73e629b8227e0e37
algorithmzuo <algorithmzuo@163.com>
2023-01-10 12:59:12 +0800

    modify code

--- src/class_2023_01_2_week/Code03_LeastOperatorsToExpressNumber.java
+++ src/class_2023_01_2_week/Code03_LeastOperatorsToExpressNumber.java
@@ -2,6 +2,17 @@ package class_2023_01_2_week;
 
 import java.util.HashMap;
 
+// 给定一个正整数 x，我们将会写出一个形如 x (op1) x (op2) x (op3) x ... 的表达式
+// 其中每个运算符 op1，op2，… 可以是加、减、乘、除之一
+// 例如，对于 x = 3，我们可以写出表达式 3 * 3 / 3 + 3 - 3，该式的值为3
+// 在写这样的表达式时，我们需要遵守下面的惯例：
+// 除运算符（/）返回有理数
+// 任何地方都没有括号
+// 我们使用通常的操作顺序：乘法和除法发生在加法和减法之前
+// 不允许使用一元否定运算符（-）。例如，“x - x” 是一个有效的表达
+// 因为它只使用减法，但是 “-x + x” 不是，因为它使用了否定运算符
+// 我们希望编写一个能使表达式等于给定的目标值 target 且运算符最少的表达式
+// 返回所用运算符的最少数量
 // 测试链接 : https://leetcode.cn/problems/least-operators-to-express-number/
 public class Code03_LeastOperatorsToExpressNumber {
 
