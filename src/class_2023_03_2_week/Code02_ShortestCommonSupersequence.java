989716db6f9223f96dba834b65172dbe071eca98
algorithmzuo <algorithmzuo@163.com>
2023-02-14 16:58:55 +0800

    modify code

---
+++ src/class_2023_03_2_week/Code02_ShortestCommonSupersequence.java
@@ -0,0 +1,44 @@
+package class_2023_03_2_week;
+
+// 测试链接 : https://leetcode.cn/problems/shortest-common-supersequence/
+public class Code02_ShortestCommonSupersequence {
+
+	public static String shortestCommonSupersequence(String str1, String str2) {
+		int n = str1.length();
+		int m = str2.length();
+		int[][] dp = new int[n + 1][m + 1];
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+				if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+					dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
+				}
+			}
+		}
+		char[] ans = new char[n + m - dp[n][m]];
+		int ansi = ans.length - 1;
+		int i = n;
+		int j = m;
+		while (i > 0 && j > 0) {
+			if (dp[i][j] == dp[i - 1][j - 1] + 1 && str1.charAt(i - 1) == str2.charAt(j - 1)) {
+				ans[ansi--] = str1.charAt(i - 1);
+				i--;
+				j--;
+			} else if (dp[i][j] == dp[i - 1][j]) {
+				ans[ansi--] = str1.charAt(i - 1);
+				i--;
+			} else {
+				ans[ansi--] = str2.charAt(j - 1);
+				j--;
+			}
+		}
+		for (; i > 0; i--) {
+			ans[ansi--] = str1.charAt(i - 1);
+		}
+		for (; j > 0; j--) {
+			ans[ansi--] = str2.charAt(j - 1);
+		}
+		return String.valueOf(ans);
+	}
+
+}
