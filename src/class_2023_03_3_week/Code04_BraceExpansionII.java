2c60a41234e7272c652134061bc302c92069b6b5
algorithmzuo <algorithmzuo@163.com>
2023-03-01 18:21:37 +0800

    modify code

---
+++ src/class_2023_03_3_week/Code04_BraceExpansionII.java
@@ -0,0 +1,73 @@
+package class_2023_03_3_week;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+
+// 测试链接 : https://leetcode.cn/problems/brace-expansion-ii/
+public class Code04_BraceExpansionII {
+
+	public static List<String> braceExpansionII(String expression) {
+		return new ArrayList<>(process(expression.toCharArray(), 0).ans);
+	}
+
+	public static class Info {
+		public TreeSet<String> ans;
+		public int end;
+
+		public Info(TreeSet<String> a, int e) {
+			ans = a;
+			end = e;
+		}
+	}
+
+	public static Info process(char[] exp, int start) {
+		TreeSet<String> ans = new TreeSet<>();
+		List<TreeSet<String>> parts = new ArrayList<>();
+		StringBuilder builder = new StringBuilder();
+		while (start != exp.length && exp[start] != '}') {
+			if (exp[start] == '{') {
+				addStringToParts(builder, parts);
+				Info next = process(exp, start + 1);
+				parts.add(next.ans);
+				start = next.end + 1;
+			} else if (exp[start] == ',') {
+				addStringToParts(builder, parts);
+				addPartsToSet(ans, parts);
+				start++;
+				parts.clear();
+			} else {
+				builder.append(exp[start]);
+				start++;
+			}
+		}
+		addStringToParts(builder, parts);
+		addPartsToSet(ans, parts);
+		return new Info(ans, start);
+	}
+
+	public static void addStringToParts(StringBuilder builder, List<TreeSet<String>> parts) {
+		if (builder.length() != 0) {
+			parts.add(new TreeSet<>());
+			parts.get(parts.size() - 1).add(builder.toString());
+			builder.delete(0, builder.length());
+		}
+	}
+
+	public static void addPartsToSet(TreeSet<String> ans, List<TreeSet<String>> parts) {
+		process(parts, 0, "", ans);
+	}
+
+	public static void process(List<TreeSet<String>> list, int i, String path, TreeSet<String> ans) {
+		if (i == list.size()) {
+			if (!path.equals("")) {
+				ans.add(path);
+			}
+		} else {
+			for (String cur : list.get(i)) {
+				process(list, i + 1, path + cur, ans);
+			}
+		}
+	}
+
+}
---
+++ src/class_2023_03_3_week/Code05_ParsingBooleanExpression.java
@@ -0,0 +1,64 @@
+package class_2023_03_3_week;
+
+// 测试链接 : https://leetcode.cn/problems/parsing-a-boolean-expression/
+public class Code05_ParsingBooleanExpression {
+
+	public static boolean parseBoolExpr(String expression) {
+		return process(expression.toCharArray(), 0).ans;
+	}
+
+	public static class Info {
+		public boolean ans;
+		public int end;
+
+		public Info(boolean a, int e) {
+			ans = a;
+			end = e;
+		}
+	}
+
+	public static Info process(char[] exp, int index) {
+		char judge = exp[index];
+		if (judge == 'f') {
+			return new Info(false, index);
+		} else if (judge == 't') {
+			return new Info(true, index);
+		} else {
+			boolean ans;
+			index += 2;
+			if (judge == '!') {
+				Info next = process(exp, index);
+				ans = !next.ans;
+				index = next.end + 1;
+			} else if (judge == '&') {
+				ans = true;
+				while (index < exp.length && exp[index] != ')') {
+					if (exp[index] == ',') {
+						index++;
+					} else {
+						Info next = process(exp, index);
+						if (!next.ans) {
+							ans = false;
+						}
+						index = next.end + 1;
+					}
+				}
+			} else {
+				ans = false;
+				while (index < exp.length && exp[index] != ')') {
+					if (exp[index] == ',') {
+						index++;
+					} else {
+						Info next = process(exp, index);
+						if (next.ans) {
+							ans = true;
+						}
+						index = next.end + 1;
+					}
+				}
+			}
+			return new Info(ans, index);
+		}
+	}
+
+}
