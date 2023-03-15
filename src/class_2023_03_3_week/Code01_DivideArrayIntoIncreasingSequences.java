cc5df4831671eb38242ce381ef5863f812537073
algorithmzuo <algorithmzuo@163.com>
2023-03-15 18:14:21 +0800

    modify code

--- src/class_2023_03_3_week/Code01_DivideArrayIntoIncreasingSequences.java
+++ src/class_2023_03_3_week/Code01_DivideArrayIntoIncreasingSequences.java
@@ -1,5 +1,7 @@
 package class_2023_03_3_week;
 
+// 给你一个 非递减 的正整数数组 nums 和整数 K
+// 判断该数组是否可以被分成一个或几个 长度至少 为 K 的 不相交的递增子序列
 // 测试链接 : https://leetcode.cn/problems/divide-array-into-increasing-sequences/
 public class Code01_DivideArrayIntoIncreasingSequences {
 
