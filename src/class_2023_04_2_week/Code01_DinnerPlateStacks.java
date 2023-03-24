ae5ec12c49f8840a29673d45b55f371052b09b40
algorithmzuo <algorithmzuo@163.com>
2023-03-24 15:42:24 +0800

    modify code

--- src/class_2023_04_2_week/Code01_DinnerPlateStacks.java
+++ src/class_2023_04_2_week/Code01_DinnerPlateStacks.java
@@ -1,152 +1,76 @@
 package class_2023_04_2_week;
 
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 // 测试链接 : https://leetcode.cn/problems/dinner-plate-stacks/
 public class Code01_DinnerPlateStacks {
 
 	class DinnerPlates {
-
-		public static class IndexTree {
-			private int[] tree;
-			private int N;
-
-			public IndexTree(int size) {
-				N = size;
-				tree = new int[N + 1];
-			}
-
-			public int sum(int index) {
-				int ret = 0;
-				while (index > 0) {
-					ret += tree[index];
-					index -= index & -index;
-				}
-				return ret;
-			}
-
-			public void add(int index, int d) {
-				while (index <= N) {
-					tree[index] += d;
-					index += index & -index;
-				}
-			}
-		}
-
-		public static class Stacks {
-			public ArrayList<Integer> heads;
-			public ArrayList<Integer> values;
-			public ArrayList<Integer> lasts;
-			public ArrayList<Integer> frees;
-			public int occupySize;
-			public int freeSize;
-
-			public Stacks(int stackSize) {
-				heads = new ArrayList<>();
-				for (int i = 0; i < stackSize; i++) {
-					heads.add(-1);
-				}
-				values = new ArrayList<>();
-				lasts = new ArrayList<>();
-				frees = new ArrayList<>();
-				occupySize = 0;
-				freeSize = 0;
-			}
-
-			public void push(int num, int stackIndex) {
-				int headIndex = heads.get(stackIndex);
-				if (freeSize == 0) {
-					heads.set(stackIndex, occupySize++);
-					values.add(num);
-					lasts.add(headIndex);
-				} else {
-					int freeIndex = frees.get(--freeSize);
-					heads.set(stackIndex, freeIndex);
-					values.set(freeIndex, num);
-					lasts.set(freeIndex, headIndex);
-				}
-			}
-
-			public int pop(int stackIndex) {
-				int headIndex = heads.get(stackIndex);
-				int ans = values.get(headIndex);
-				int newHeadIndex = lasts.get(headIndex);
-				heads.set(stackIndex, newHeadIndex);
-				if (freeSize >= frees.size()) {
-					frees.add(headIndex);
-					freeSize++;
-				} else {
-					frees.set(freeSize++, headIndex);
-				}
-				return ans;
-			}
-
-		}
-
-		private static final int MAXN = 100000;
-
-		Stacks ss;
-
-		IndexTree it;
-
+		private final int N = 100001;
+		private int[] cnt = new int[N + 1];
+		private ArrayList<ArrayList<Integer>> stacks = new ArrayList<>();
+		private PriorityQueue<Integer> heap = new PriorityQueue<>();
 		private int capacity;
+		private int size;
 
 		public DinnerPlates(int cap) {
-			ss = new Stacks(MAXN + 1);
-			it = new IndexTree(MAXN + 1);
 			capacity = cap;
-		}
-
-		public int popAtStack(int index) {
-			if (index < 0 || it.sum(index + 1) - it.sum(index) == 0) {
-				return -1;
-			}
-			it.add(index + 1, -1);
-			return ss.pop(index);
+			size = 0;
 		}
 
 		public void push(int val) {
-			int index = leftHasSpace();
-			it.add(index + 1, 1);
-			ss.push(val, index);
+			if (!heap.isEmpty()) {
+				while (cnt[size] == 0 && size > 0) {
+					size--;
+				}
+				if (heap.peek() >= size) {
+					while (!heap.isEmpty() && heap.peek() > size) {
+						heap.poll();
+					}
+					stacks.get(size).add(val);
+					cnt[size]++;
+				} else {
+					int cur = heap.peek();
+					cnt[cur]++;
+					stacks.get(cur).add(val);
+					if (cnt[cur] == capacity) {
+						heap.poll();
+					}
+				}
+			} else {
+				if (cnt[size] == capacity && size < N) {
+					size++;
+				}
+				if (cnt[size] == 0 && stacks.size() <= size) {
+					stacks.add(new ArrayList<>());
+				}
+				stacks.get(size).add(val);
+				cnt[size]++;
+			}
 		}
 
 		public int pop() {
-			return popAtStack(rightHasData());
+			if (size == 0 && cnt[size] == 0) {
+				return -1;
+			}
+			while (cnt[size] == 0 && size > 0) {
+				size--;
+			}
+			return stacks.get(size).remove(--cnt[size]);
 		}
 
-		private int leftHasSpace() {
-			int l = 0;
-			int r = MAXN;
-			int m, ans = r;
-			while (l <= r) {
-				m = (l + r) / 2;
-				if (it.sum(m + 1) < (m + 1) * capacity) {
-					ans = m;
-					r = m - 1;
-				} else {
-					l = m + 1;
-				}
+		public int popAtStack(int index) {
+			if (cnt[index] == 0) {
+				return -1;
 			}
+			int ans = stacks.get(index).remove(cnt[index] - 1);
+			if (cnt[index] == capacity) {
+				heap.offer(index);
+			}
+			cnt[index]--;
 			return ans;
 		}
-
-		private int rightHasData() {
-			int l = 0;
-			int r = MAXN;
-			int m, ans = r;
-			while (l <= r) {
-				m = (l + r) / 2;
-				if (it.sum(r + 1) - it.sum(m) > 0) {
-					ans = m;
-					l = m + 1;
-				} else {
-					r = m - 1;
-				}
-			}
-			return ans;
-		}
-
 	}
 
 }
