5db6d9248ac4e2155df72010ca404283b0c6a7b9
algorithmzuo <algorithmzuo@163.com>
2023-04-24 19:57:02 +0800

    modify code

--- src/class_2023_05_2_week/Code01_MagicScrollProbelm.java
+++ src/class_2023_05_2_week/Code01_MagicScrollProbelm.java
@@ -9,17 +9,100 @@ package class_2023_05_2_week;
 // -100000 <= arr里的值 <= 100000
 public class Code01_MagicScrollProbelm {
 
-//	public static int maxSum1(int[] arr) {
-//		int p1 = 0;
-//		for (int num : arr) {
-//			p1 += num;
-//		}
-//
-//	}
-//
-//	public static int query1(int[] arr, int L) {
-//		int n = arr.length;
-//		int[] sum = new int[n];
-//	}
+	// 暴力方法
+	// 为了测试
+	public static int maxSum1(int[] arr) {
+		int p1 = 0;
+		for (int num : arr) {
+			p1 += num;
+		}
+		int n = arr.length;
+		int p2 = mustOneScroll(arr, 0, n - 1);
+		int p3 = Integer.MIN_VALUE;
+		for (int i = 1; i < n; i++) {
+			p3 = Math.max(p3, mustOneScroll(arr, 0, i - 1) + mustOneScroll(arr, i, n - 1));
+		}
+		return Math.max(p1, Math.max(p2, p3));
+	}
+
+	// 为了测试
+	public static int mustOneScroll(int[] arr, int L, int R) {
+		int ans = Integer.MIN_VALUE;
+		for (int a = L; a <= R; a++) {
+			for (int b = a; b <= R; b++) {
+				int curAns = 0;
+				for (int i = L; i < a; i++) {
+					curAns += arr[i];
+				}
+				for (int i = b + 1; i <= R; i++) {
+					curAns += arr[i];
+				}
+				ans = Math.max(ans, curAns);
+			}
+		}
+		return ans;
+	}
+
+	// 正式方法
+	// 时间复杂度O(N)
+	public static int maxSum2(int[] arr) {
+		if (arr.length == 0) {
+			return 0;
+		}
+		int p1 = 0;
+		for (int num : arr) {
+			p1 += num;
+		}
+		int n = arr.length;
+		int[] left = new int[n];
+		int sum = arr[0];
+		int maxSum = Math.max(0, sum);
+		for (int i = 1; i < n; i++) {
+			left[i] = Math.max(left[i - 1] + arr[i], maxSum);
+			sum += arr[i];
+			maxSum = Math.max(maxSum, sum);
+		}
+		int p2 = left[n - 1];
+		int[] right = new int[n];
+		sum = arr[n - 1];
+		maxSum = Math.max(0, sum);
+		for (int i = n - 2; i >= 0; i--) {
+			right[i] = Math.max(arr[i] + right[i + 1], maxSum);
+			sum += arr[i];
+			maxSum = Math.max(maxSum, sum);
+		}
+		int p3 = Integer.MIN_VALUE;
+		for (int i = 1; i < n; i++) {
+			p3 = Math.max(p3, left[i - 1] + right[i]);
+		}
+		return Math.max(p1, Math.max(p2, p3));
+	}
+
+	// 为了测试
+	public static int[] randomArray(int n, int v) {
+		int[] ans = new int[n];
+		for (int i = 0; i < n; i++) {
+			ans[i] = (int) (Math.random() * (v * 2 + 1)) - v;
+		}
+		return ans;
+	}
+
+	// 为了测试
+	public static void main(String[] args) {
+		int N = 50;
+		int V = 100;
+		int testTimes = 10000;
+		System.out.println("测试开始");
+		for (int i = 0; i < testTimes; i++) {
+			int n = (int) (Math.random() * N);
+			int[] arr = randomArray(n, V);
+			int ans1 = maxSum1(arr);
+			int ans2 = maxSum2(arr);
+			if (ans1 != ans2) {
+				System.out.println("出错了!");
+			}
+		}
+		System.out.println("测试结束");
+	}
 
 }
--- src/class_2023_05_2_week/Code02_ValidRangeBetweenTwoArrays.java
+++ src/class_2023_05_2_week/Code02_ValidRangeBetweenTwoArrays.java
@@ -11,9 +11,106 @@ package class_2023_05_2_week;
 // 第一行有一个正整数N(1<=N<=100000)，代表两个数组的长度。
 // 第二行有N个非负整数，范围在0到1000000000之间，代表数组中的元素。
 // 第三行有N个非负整数，范围在0到1000000000之间，代表数组中的元素。
-// 第四行有4个整数La,Ra,Lb,Rb，范围在0到1018之间，代表题目描述中的参数。
+// 第四行有4个整数La,Ra,Lb,Rb，范围在0到10^18之间，代表题目描述中的参数。
 // 输出
 // 输出一个整数，代表所求的答案。
 public class Code02_ValidRangeBetweenTwoArrays {
+
+	// 暴力方法
+	// 为了测试
+	public static int nums1(int[] A, int[] B, int la, int ra, int lb, int rb) {
+		int n = A.length;
+		int ans = 0;
+		for (int l = 0; l < n; l++) {
+			for (int r = l; r < n; r++) {
+				int sumA = 0;
+				int sumB = 0;
+				for (int i = l; i <= r; i++) {
+					sumA += A[i];
+					sumB += B[i];
+				}
+				if (sumA >= la && sumA <= ra && sumB >= lb && sumB <= rb) {
+					ans++;
+				}
+			}
+		}
+		return ans;
+	}
+
+	// 正式方法
+	// 时间复杂度O(N)
+	public static int nums2(int[] A, int[] B, int la, int ra, int lb, int rb) {
+		int n = A.length;
+		int ans = 0;
+		int rightA1 = 0, sumA1 = 0, rightA2 = 0, sumA2 = 0, rightB1 = 0, sumB1 = 0, rightB2 = 0, sumB2 = 0;
+		for (int l = 0; l < n; l++) {
+			while (rightA1 < n && sumA1 + A[rightA1] < la) {
+				sumA1 += A[rightA1++];
+			}
+			while (rightA2 < n && sumA2 + A[rightA2] <= ra) {
+				sumA2 += A[rightA2++];
+			}
+			while (rightB1 < n && sumB1 + B[rightB1] < lb) {
+				sumB1 += B[rightB1++];
+			}
+			while (rightB2 < n && sumB2 + B[rightB2] <= rb) {
+				sumB2 += B[rightB2++];
+			}
+			int left = Math.max(rightA1, rightB1);
+			int right = Math.min(rightA2, rightB2);
+			if (left < right) {
+				ans += right - left;
+			}
+			if (rightA1 == l) {
+				rightA1++;
+			} else {
+				sumA1 -= A[l];
+			}
+			sumA2 -= A[l];
+			if (rightB1 == l) {
+				rightB1++;
+			} else {
+				sumB1 -= B[l];
+			}
+			sumB2 -= B[l];
+		}
+		return ans;
+	}
+
+	// 为了测试
+	public static int[] randomArray(int n, int v) {
+		int[] ans = new int[n];
+		for (int i = 0; i < n; i++) {
+			ans[i] = (int) (Math.random() * v);
+		}
+		return ans;
+	}
+
+	// 为了测试
+	public static void main(String[] args) {
+		int N = 50;
+		int V = 100;
+		int testTimes = 10000;
+		System.out.println("测试开始");
+		for (int i = 0; i < testTimes; i++) {
+			int n = (int) (Math.random() * N);
+			int[] A = randomArray(n, V);
+			int[] B = randomArray(n, V);
+			int a = (int) (Math.random() * V);
+			int b = (int) (Math.random() * V);
+			int c = (int) (Math.random() * V);
+			int d = (int) (Math.random() * V);
+			int la = Math.min(a, b);
+			int ra = Math.max(a, b);
+			int lb = Math.min(c, d);
+			int rb = Math.max(c, d);
+			int ans1 = nums1(A, B, la, ra, lb, rb);
+			int ans2 = nums2(A, B, la, ra, lb, rb);
+			if (ans1 != ans2) {
+				System.out.println("出错了!");
+			}
+		}
+		System.out.println("测试结束");
+	}
 
 }
