32f4ffa500b70c86ec58556b0bdac8aa320df42f
algorithmzuo <algorithmzuo@163.com>
2023-05-24 19:51:45 +0800

    modify code

--- src/class_2023_05_4_week/Code02_SplitNtoKMaxProduct.java
+++ src/class_2023_05_4_week/Code02_SplitNtoKMaxProduct.java
@@ -3,6 +3,8 @@ package class_2023_05_4_week;
 // 来自华为
 // 一个数字n，一定要分成k份
 // 得到的乘积尽量大是多少
+// 数字n和k，可能非常大，到达10^12规模
+// 结果可能更大，所以返回结果对1000000007取模
 public class Code02_SplitNtoKMaxProduct {
 
 	// 暴力递归
@@ -57,12 +59,42 @@ public class Code02_SplitNtoKMaxProduct {
 		return ans;
 	}
 
+	// 贪心的解
+	// 这是最优解
+	// 但是如果结果很大，让求余数...
+	public static int maxValue3(long n, long k) {
+		if (k == 0 || n < k) {
+			return -1;
+		}
+		int mod = 1000000007;
+		long a = n / k;
+		long b = n % k;
+		long part1 = power(a + 1, b, mod);
+		long part2 = power(a, k - b, mod);
+		return (int) (part1 * part2) % mod;
+	}
+
+	// 返回a的n次方，%mod的结果
+	public static long power(long a, long n, int mod) {
+		long ans = 1;
+		long tmp = a;
+		while (n != 0) {
+			if ((n & 1) != 0) {
+				ans = (ans * tmp) % mod;
+			}
+			n >>= 1;
+			tmp = (tmp * tmp) % mod;
+		}
+		return ans;
+	}
+
 	public static void main(String[] args) {
 		// 可以自己来用参数实验
 		int n = 13;
 		int k = 4;
 		System.out.println(maxValue1(n, k));
 		System.out.println(maxValue2(n, k));
+		System.out.println(maxValue3(n, k));
 	}
 
 }
