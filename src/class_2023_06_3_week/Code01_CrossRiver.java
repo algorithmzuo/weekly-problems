e9a9c6e33f79de7b1c465ed1b3e247ba9e0fa9a9
algorithmzuo <algorithmzuo@163.com>
2023-06-20 17:23:50 +0800

    modify code

--- src/class_2023_06_3_week/Code01_CrossRiver.java
+++ src/class_2023_06_3_week/Code01_CrossRiver.java
@@ -64,7 +64,9 @@ public class Code01_CrossRiver {
 			dp[2] = arr[0] + arr[1] + arr[2];
 		}
 		for (int i = 3; i < n; i++) {
-			dp[i] = Math.min(dp[i - 2] + arr[0] + 2 * arr[1] + arr[i], dp[i - 1] + arr[i] + arr[0]);
+			dp[i] = Math.min(
+					dp[i - 2] + arr[1] + arr[0] + arr[i] + arr[1],
+					dp[i - 1] + arr[i] + arr[0]);
 		}
 		return dp[n - 1];
 	}
