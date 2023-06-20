8e3d6953f6e51c19486ad015d942a3d4f3bac824
algorithmzuo <algorithmzuo@163.com>
2023-06-20 16:16:18 +0800

    modify code

--- src/class_2023_06_3_week/Code01_CrossRiver.java
+++ src/class_2023_06_3_week/Code01_CrossRiver.java
@@ -1,5 +1,22 @@
 package class_2023_06_3_week;
 
+// 来自华为OD
+// 一支n个士兵的军队正在趁夜色逃亡，途中遇到一条湍急的大河
+// 敌军在T的时长后到达河面，没到过对岸的士兵都会被消灭
+// 现在军队只找到了1只小船，这船最多能同时坐上2个士兵。
+// 1) 当1个士兵划船过河，用时为a[i]
+// 2) 当2个士兵坐船同时划船过河时, 用时为max(a[j],a[i])两士兵中用时最长的
+// 3) 当2个士兵坐船只有1个士兵划船时, 用时为a[i] * 10, a[i]为划船士兵用时
+// 请帮忙给出一种解决方案，保证存活的士兵最多，且过河用时最短
+// 我们先看一下如下的题，再讲一下华为OD的扩展
+// 测试链接 : https://www.luogu.com.cn/problem/P1809
+// 有一个大晴天, Oliver与同学们一共N人出游, 他们走到一条河的东岸边，想要过河到西岸
+// 而东岸边有一条小船。船太小了，一次只能乘坐两人，每个人都有一个渡河时间T
+// 船划到对岸的时间等于船上渡河时间较长的人所用时间
+// 现在已知N个人的渡河时间Ti
+// Oliver 想要你告诉他，他们最少要花费多少时间，才能使所有人都过河
+// 注意，只有船在东岸（西岸）的人才能坐上船划到对岸。
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,8 +25,6 @@ import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 import java.util.Arrays;
 
-// 除了这个题之外，再讲一下华为OD的扩展
-// 测试链接 : https://www.luogu.com.cn/problem/P1809
 public class Code01_CrossRiver {
 
 	public static int MAXN = 100001;
@@ -39,21 +54,19 @@ public class Code01_CrossRiver {
 
 	public static int minCost() {
 		Arrays.sort(arr, 0, n);
-		if (n == 1) {
-			return arr[0];
-		} else if (n == 2) {
-			return arr[1];
-		} else if (n == 3) {
-			return arr[0] + arr[1] + arr[2];
-		} else {
+		if (n >= 1) {
 			dp[0] = arr[0];
-			dp[1] = arr[1];
-			dp[2] = arr[0] + arr[1] + arr[2];
-			for (int i = 3; i < n; i++) {
-				dp[i] = Math.min(dp[i - 2] + arr[0] + 2 * arr[1] + arr[i], dp[i - 1] + arr[i] + arr[0]);
-			}
-			return dp[n - 1];
 		}
+		if (n >= 2) {
+			dp[1] = arr[1];
+		}
+		if (n >= 3) {
+			dp[2] = arr[0] + arr[1] + arr[2];
+		}
+		for (int i = 3; i < n; i++) {
+			dp[i] = Math.min(dp[i - 2] + arr[0] + 2 * arr[1] + arr[i], dp[i - 1] + arr[i] + arr[0]);
+		}
+		return dp[n - 1];
 	}
 
 }
--- src/class_2023_06_3_week/Code06_SpecialPermutations.java
+++ src/class_2023_06_3_week/Code06_SpecialPermutations.java
@@ -1,5 +1,11 @@
 package class_2023_06_3_week;
 
+// 给你一个下标从 0 开始的整数数组 nums ，它包含 n 个 互不相同 的正整数
+// 如果 nums 的一个排列满足以下条件，我们称它是一个特别的排列
+// 对于 0 <= i < n - 1 的下标 i
+// 要么 nums[i] % nums[i+1] == 0
+// 要么 nums[i+1] % nums[i] == 0
+// 请你返回特别排列的总数目，由于答案可能很大，请将它对 1000000007 取余 后返回
 // 测试链接 : https://leetcode.cn/problems/special-permutations/
 public class Code06_SpecialPermutations {
 
