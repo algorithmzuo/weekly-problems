b12f20362f285642de2e40f7e86514de4523911c
algorithmzuo <algorithmzuo@163.com>
2023-06-26 21:04:41 +0800

    modify code

--- src/class_2023_06_4_week/Code02_PaintingTheWalls.java
+++ src/class_2023_06_4_week/Code02_PaintingTheWalls.java
@@ -2,6 +2,13 @@ package class_2023_06_4_week;
 
 import java.util.Arrays;
 
+// 给你两个长度为 n 下标从 0 开始的整数数组 cost 和 time
+// 分别表示给 n 堵不同的墙刷油漆需要的开销和时间。你有两名油漆匠
+// 一位需要 付费 的油漆匠，刷第 i 堵墙需要花费 time[i] 单位的时间
+// 开销为 cost[i] 单位的钱。
+// 一位 免费 的油漆匠，刷 任意 一堵墙的时间为 1 单位，开销为 0
+// 但是必须在付费油漆匠 工作 时，免费油漆匠才会工作
+// 请你返回刷完 n 堵墙最少开销为多少
 // 测试链接 : https://leetcode.cn/problems/painting-the-walls/
 public class Code02_PaintingTheWalls {
 
