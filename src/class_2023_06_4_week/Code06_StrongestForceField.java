5667cdb6abfe0ab01ca647a7a919e1f05f760ef5
algorithmzuo <algorithmzuo@163.com>
2023-06-28 19:22:24 +0800

    modify code

--- src/class_2023_06_4_week/Code06_StrongestForceField.java
+++ src/class_2023_06_4_week/Code06_StrongestForceField.java
@@ -1,9 +1,17 @@
-package class_2023_07_1_week;
+package class_2023_06_4_week;
 
 import java.util.Arrays;
 
+// 小扣在探索丛林的过程中，无意间发现了传说中"落寞的黄金之都"
+// 而在这片建筑废墟的地带中，小扣使用探测仪监测到了存在某种带有「祝福」效果的力场
+// 经过不断的勘测记录，小扣将所有力场的分布都记录了下来
+// forceField[i] = [x,y,side] 
+// 表示第 i 片力场将覆盖以坐标 (x,y) 为中心，边长为 side 的正方形区域。
+// 若任意一点的 力场强度 等于覆盖该点的力场数量
+// 请求出在这片地带中 力场强度 最强处的 力场强度
+// 注意：力场范围的边缘同样被力场覆盖。
 // 测试链接 : https://leetcode.cn/problems/xepqZ5/
-public class Code02_StrongestForceField {
+public class Code06_StrongestForceField {
 
 	public static int fieldOfGreatestBlessing(int[][] fields) {
 		int n = fields.length;
--- src/class_2023_07_1_week/Code02_FindValidMatrixGivenRowAndColumnSums.java
+++ src/class_2023_07_1_week/Code02_FindValidMatrixGivenRowAndColumnSums.java
@@ -1,6 +1,6 @@
-package class_2023_07_2_week;
+package class_2023_07_1_week;
 
 // 测试链接 : https://leetcode.cn/problems/find-valid-matrix-given-row-and-column-sums/
-public class Code03_FindValidMatrixGivenRowAndColumnSums {
+public class Code02_FindValidMatrixGivenRowAndColumnSums {
 
 }
