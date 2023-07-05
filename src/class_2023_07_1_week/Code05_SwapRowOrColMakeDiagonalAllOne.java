556156e2ad9626e872086c49717173f50800a5a5
algorithmzuo <algorithmzuo@163.com>
2023-07-05 19:39:02 +0800

    modify code

--- src/class_2023_07_1_week/Code05_SwapRowOrColMakeDiagonalAllOne.java
+++ src/class_2023_07_1_week/Code05_SwapRowOrColMakeDiagonalAllOne.java
@@ -1,7 +1,7 @@
 package class_2023_07_1_week;
 
 // 来自网易
-// 题目出处 : https://leetcode-cn.com/circle/discuss/uOnnUA/
+// 题目出处 : https://leetcode.cn/circle/discuss/uOnnUA/
 // 已知一个n*n的01矩阵，
 // 只能通过通过行交换、或者列交换的方式调整矩阵，
 // 判断这个矩阵的对角线是否能全为1，如果能返回true，不能返回false
