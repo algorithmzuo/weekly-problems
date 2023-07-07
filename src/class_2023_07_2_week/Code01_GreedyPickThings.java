80421f9ab4e187b056535787ae4954de289f6bb5
algorithmzuo <algorithmzuo@163.com>
2023-07-08 00:54:37 +0800

    modify code

--- src/class_2023_07_2_week/Code01_GreedyPickThings.java
+++ src/class_2023_07_2_week/Code01_GreedyPickThings.java
@@ -8,6 +8,12 @@ import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 import java.util.Arrays;
 
+// 阿里巴巴走进了装满宝藏的藏宝洞。藏宝洞里面有N堆金币
+// 第i堆金币的总重量和总价值分别是m[i]、v[i]
+// 阿里巴巴有一个承重量为T的背包，但并不一定有办法将全部的金币都装进去
+// 他想装走尽可能多价值的金币
+// 所有金币都可以随意分割，分割完的金币重量价值比（也就是单位价格）不变
+// 请问阿里巴巴最多可以拿走多少价值的金币？
 // 测试链接 : https://www.luogu.com.cn/problem/P2240
 public class Code01_GreedyPickThings {
 
--- src/class_2023_07_2_week/Code02_FindValidMatrixGivenRowAndColumnSums.java
+++ src/class_2023_07_2_week/Code02_FindValidMatrixGivenRowAndColumnSums.java
@@ -1,5 +1,12 @@
 package class_2023_07_2_week;
 
+// 给你两个非负整数数组 rowSum 和 colSum
+// 其中 rowSum[i] 是二维矩阵中第 i 行元素的和
+// colSum[j] 是第 j 列元素的和，换言之你不知道矩阵里的每个元素
+// 但是你知道每一行和每一列的和
+// 请找到大小为 rowSum.length x colSum.length 的任意 非负整数 矩阵
+// 且该矩阵满足 rowSum 和 colSum 的要求
+// 请你返回任意一个满足题目要求的二维矩阵，题目保证存在 至少一个 可行矩阵。
 // 测试链接 : https://leetcode.cn/problems/find-valid-matrix-given-row-and-column-sums/
 public class Code02_FindValidMatrixGivenRowAndColumnSums {
 
