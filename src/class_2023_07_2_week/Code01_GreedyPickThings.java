2426643bb18f2d3ce045f2b6bc7b999fd7c56274
algorithmzuo <algorithmzuo@163.com>
2023-07-08 00:57:11 +0800

    modify code

--- src/class_2023_07_2_week/Code01_GreedyPickThings.java
+++ src/class_2023_07_2_week/Code01_GreedyPickThings.java
@@ -1,5 +1,16 @@
 package class_2023_07_2_week;
 
+// 阿里巴巴走进了装满宝藏的藏宝洞。藏宝洞里面有N堆金币
+// 第i堆金币的总重量和总价值分别是m[i]、v[i]
+// 阿里巴巴有一个承重量为T的背包，但并不一定有办法将全部的金币都装进去
+// 他想装走尽可能多价值的金币
+// 所有金币都可以随意分割，分割完的金币重量价值比（也就是单位价格）不变
+// 请问阿里巴巴最多可以拿走多少价值的金币？
+// 测试链接 : https://www.luogu.com.cn/problem/P2240
+// 请同学们务必参考如下代码中关于输入、输出的处理
+// 这是输入输出处理效率很高的写法
+// 提交以下的code，提交时请把类名改成"Main"，可以直接通过
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,13 +19,6 @@ import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 import java.util.Arrays;
 
-// 阿里巴巴走进了装满宝藏的藏宝洞。藏宝洞里面有N堆金币
-// 第i堆金币的总重量和总价值分别是m[i]、v[i]
-// 阿里巴巴有一个承重量为T的背包，但并不一定有办法将全部的金币都装进去
-// 他想装走尽可能多价值的金币
-// 所有金币都可以随意分割，分割完的金币重量价值比（也就是单位价格）不变
-// 请问阿里巴巴最多可以拿走多少价值的金币？
-// 测试链接 : https://www.luogu.com.cn/problem/P2240
 public class Code01_GreedyPickThings {
 
 	public static int MAXN = 101;
