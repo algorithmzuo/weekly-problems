9d1af79d84cb208ffea8a91f75ea68316b92124e
algorithmzuo <algorithmzuo@163.com>
2023-07-12 16:33:35 +0800

    modify code

--- src/class_2023_07_2_week/Code04_MaximumEmployeesToBeInvitedToAMeeting.java
+++ src/class_2023_07_2_week/Code04_MaximumEmployeesToBeInvitedToAMeeting.java
@@ -1,6 +1,13 @@
 package class_2023_07_2_week;
 
-// 测试链接 : https://leetcode.com/problems/maximum-employees-to-be-invited-to-a-meeting/
+// 一个公司准备组织一场会议，邀请名单上有 n 位员工
+// 公司准备了一张 圆形 的桌子，可以坐下 任意数目 的员工
+// 员工编号为 0 到 n - 1 。每位员工都有一位 喜欢 的员工
+// 每位员工 当且仅当 他被安排在喜欢员工的旁边，他才会参加会议
+// 每位员工喜欢的员工 不会 是他自己。
+// 给你一个下标从 0 开始的整数数组 favorite
+// 其中 favorite[i] 表示第 i 位员工喜欢的员工。请你返回参加会议的 最多员工数目
+// 测试链接 : https://leetcode.cn/problems/maximum-employees-to-be-invited-to-a-meeting/
 public class Code04_MaximumEmployeesToBeInvitedToAMeeting {
 
 	public static int maximumInvitations(int[] favorite) {
