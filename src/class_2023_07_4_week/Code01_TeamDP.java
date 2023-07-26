cf098ef1a4e7af3aca164ebd37bc85a5bd732128
algorithmzuo <algorithmzuo@163.com>
2023-07-26 13:19:19 +0800

    modify code

--- src/class_2023_07_4_week/Code01_TeamDP.java
+++ src/class_2023_07_4_week/Code01_TeamDP.java
@@ -1,6 +1,12 @@
 package class_2023_07_4_week;
 
+// 自 01背包问世之后，小 A 对此深感兴趣
+// 一天，小 A 去远游，却发现他的背包不同于 01 背包，他的物品大致可分为 k 组
+// 每组中的物品只能选择1件，现在他想知道最大的利用价值是多少
 // 测试链接 : www.luogu.com.cn/problem/P1757
+// 请同学们务必参考如下代码中关于输入、输出的处理
+// 这是输入输出处理效率很高的写法
+// 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/class_2023_07_4_week/Code03_OnesAndZeroes.java
+++ src/class_2023_07_4_week/Code03_OnesAndZeroes.java
@@ -1,5 +1,8 @@
 package class_2023_07_4_week;
 
+// 给你一个二进制字符串数组 strs 和两个整数 m 和 n
+// 请你找出并返回 strs 的最大子集的长度，该子集中 最多 有 m 个 0 和 n 个 1
+// 如果 x 的所有元素也是 y 的元素，集合 x 是集合 y 的 子集
 // 测试链接 : https://leetcode.cn/problems/ones-and-zeroes/
 public class Code03_OnesAndZeroes {
 
