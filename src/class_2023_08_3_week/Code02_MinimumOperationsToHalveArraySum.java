a036b3358570511c90d7a987721f054022df471a
algorithmzuo <algorithmzuo@163.com>
2023-07-26 16:15:50 +0800

    modify code

--- src/class_2023_08_3_week/Code02_MinimumOperationsToHalveArraySum.java
+++ src/class_2023_08_3_week/Code02_MinimumOperationsToHalveArraySum.java
@@ -23,44 +23,46 @@ public class Code02_MinimumOperationsToHalveArraySum {
 
 	public static int MAXN = 100001;
 
-	public static long[] help = new long[MAXN];
+	public static long[] heap = new long[MAXN];
+
+	public static int size;
 
 	public static int halveArray2(int[] nums) {
-		int n = nums.length;
+		size = nums.length;
 		long sum = 0;
-		for (int i = n - 1; i >= 0; i--) {
-			help[i] = (long) nums[i] << 20;
-			sum += help[i];
-			heapify(i, n);
+		for (int i = size - 1; i >= 0; i--) {
+			heap[i] = (long) nums[i] << 20;
+			sum += heap[i];
+			heapify(i);
 		}
 		sum /= 2;
 		int ans = 0;
 		for (long minus = 0; minus < sum; ans++) {
-			help[0] /= 2;
-			minus += help[0];
-			heapify(0, n);
+			heap[0] /= 2;
+			minus += heap[0];
+			heapify(0);
 		}
 		return ans;
 	}
 
-	public static void heapify(int i, int size) {
+	public static void heapify(int i) {
 		int l = i * 2 + 1;
 		while (l < size) {
-			int largest = l + 1 < size && help[l + 1] > help[l] ? l + 1 : l;
-			largest = help[largest] > help[i] ? largest : i;
-			if (largest == i) {
+			int best = l + 1 < size && heap[l + 1] > heap[l] ? l + 1 : l;
+			best = heap[best] > heap[i] ? best : i;
+			if (best == i) {
 				break;
 			}
-			swap(largest, i);
-			i = largest;
+			swap(best, i);
+			i = best;
 			l = i * 2 + 1;
 		}
 	}
 
 	public static void swap(int i, int j) {
-		long tmp = help[i];
-		help[i] = help[j];
-		help[j] = tmp;
+		long tmp = heap[i];
+		heap[i] = heap[j];
+		heap[j] = tmp;
 	}
 
 }
