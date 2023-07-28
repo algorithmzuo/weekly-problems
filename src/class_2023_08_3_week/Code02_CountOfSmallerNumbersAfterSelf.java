d7741cce0c478b0d016ee051345849cfb4f09533
algorithmzuo <algorithmzuo@163.com>
2023-07-29 00:26:05 +0800

    modify code

--- src/class_2023_08_3_week/Code02_CountOfSmallerNumbersAfterSelf.java
+++ src/class_2023_08_3_week/Code02_CountOfSmallerNumbersAfterSelf.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 // 可以用归并分治，但这里用树状数组实现
+// 如果leetcode有一天增大了数值的范围，就离散化处理
 // 测试链接 : https://leetcode.cn/problems/count-of-smaller-numbers-after-self/
 public class Code02_CountOfSmallerNumbersAfterSelf {
 
